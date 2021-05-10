a3cd69e1734310c7e06dae2a5af67be04edd355a
Puneet Virmani <>
2021-05-10 17:06:30 +0530

    CODE COMMIT

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <Text>Hello World</Text>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+});
